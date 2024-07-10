9f92b254a4795ae3f4dab0533024d4810d0fda74
SeYun <cindy21cx@gmail.com>
2024-07-10 16:47:19 +0900

    Add requirements.txt and static

--- postboxProject/postboxProject/settings.py
+++ postboxProject/postboxProject/settings.py
@@ -20,12 +20,30 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-otg1x5lhyryi(a(nl-sbjl+8!h7ydw(d!nn)be2fmq!-p8(nc+'
+# SECRET_KEY = 'django-insecure-otg1x5lhyryi(a(nl-sbjl+8!h7ydw(d!nn)be2fmq!-p8(nc+'
+
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +140,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = os.path.join('staticfiles')  #추가
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- postboxProject/postboxProject/urls.py
+++ postboxProject/postboxProject/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin1234/', admin.site.urls),
     path('', include('posts.urls')),
 ]
